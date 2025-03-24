955bcc3c27661918e164d3ef3165504b7103a187
Balabatt23 <muhammadyuki7227@gmail.com>
2025-03-24 20:33:21 +0700

    tugas pratikum 4

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ShopController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/home', [HomeController::class, 'home'])->name('home');
+Route::get('/login', [LoginController::class, 'login'])->name('login');
+Route::get('/shop', [ShopController::class, 'shop'])->name('shop');
