1b3471b52e6f4340fd629d87ad891560f300355c
Balabatt23 <muhammadyuki7227@gmail.com>
2025-04-08 14:09:19 +0700

    Haiiiii

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ShopController;
-
+use App\Http\Controllers\RegistrasiController;
 Route::get('/', function () {
     return view('welcome');
 });
@@ -12,3 +12,4 @@ Route::get('/', function () {
 Route::get('/home', [HomeController::class, 'home'])->name('home');
 Route::get('/login', [LoginController::class, 'login'])->name('login');
 Route::get('/shop', [ShopController::class, 'shop'])->name('shop');
+Route::get('/registrasi', [RegistrasiController::class, 'registrasi'])->name('registrasi');
