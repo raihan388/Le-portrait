93c51989eceaedea75de50ec5d1ceeb4da4852b8
salsyabilla <salsyabillarosa@gmail.com>
2025-04-17 10:25:51 +0700

    Update tampilan about

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ShopController;
 use App\Http\Controllers\RegistrasiController;
+use App\Http\Controllers\AboutController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -14,6 +15,7 @@ Route::get('/home', [HomeController::class, 'home'])->name('home');
 Route::get('/login', [LoginController::class, 'login'])->name('login');
 Route::get('/shop', [ShopController::class, 'shop'])->name('shop');
 Route::get('/registrasi', [RegistrasiController::class, 'registrasi'])->name('registrasi');
+Route::get('/about' , [AboutController::class, 'about'])->name('about');
 
 // Route untuk halaman kamera 3 dan 4
 Route::get('/camera/{camera}', function ($camera) {
